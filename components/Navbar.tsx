065ac2c913099b2d4e7a560993d3db94d7da5173
Jun Ukemori <jukemori@gmail.com>
2023-11-04 17:14:16 +0900

    add nav component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,29 @@ const Navbar = () => {
     <nav className="flex-center fixed top-0 z-50 w-full border-black-200 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-          <Image src="/jsm-logo.svg" alt="logo" width={55} height={40} />
+          <Image src="/jsm-logo.svg" alt="JSM logo" width={55} height={40} />
         </Link>
+
+        <Image
+          src="/hamburger-menu.svg"
+          width={30}
+          height={30}
+          alt="Hamburger menu"
+          className="block md:hidden"
+        />
+
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href="https://jsmastery.pro/next13" target="_black">
+              Next.js 13.4 Course
+            </Link>
+          </li>
+          <li className="body-text !font-normal">
+            <Link href="https://jsmastery.pro/masterclass" target="_black">
+              Masterclass
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
