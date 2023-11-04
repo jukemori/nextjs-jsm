05812c2b238d67ab6f078dd388dd719ae35e048c
Jun Ukemori <jukemori@gmail.com>
2023-11-04 12:01:44 +0900

    add nav style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
-import React from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="flex-center fixed top-0 z-50 w-full border-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+        <Link href="/">
+          <Image src="/jsm-logo.svg" alt="logo" width={55} height={40} />
+        </Link>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
